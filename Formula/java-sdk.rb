08f98709dfdaff260b2561a8cb18dcdd4b0c5d32
Indrajit Raychaudhuri <irc@indrajit.com>
2013-07-04 17:28:14 -0500

    java-sdk: Add installer options `--with-docs` and `--with-unlimited-jce` and other improvements

--- Formula/java-sdk.rb
+++ Formula/java-sdk.rb
@@ -7,17 +7,30 @@ class JavaDownloadStrategy < CurlDownloadStrategy
   end
 end
 
+class JavaDocs < Formula
+  url 'http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-apidocs.zip', :using => JavaDownloadStrategy
+  sha1 '60b9945344cfbe0867d4c5be234d9795745ec727'
+  version '1.7.0_25'
+end
+
+class UnlimitedJcePolicy < Formula
+  url 'http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip', :using => JavaDownloadStrategy
+  sha1 '7d3c9ee89536b82cd21c680088b1bced16017253'
+  version '1.7.0_25'
+end
+
 class JavaSdk < Formula
   homepage 'http://www.oracle.com/technetwork/java/javase/index.html'
   url 'http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-macosx-x64.dmg', :using => JavaDownloadStrategy
   sha1 '302164484e6d4dde1f64a658c155facc1130a1de'
   version '1.7.0_25'
 
-  keg_only "Java SDK is installed via system package installer."
+  keg_only 'Java SDK is installed via system package installer.'
 
   depends_on :macos => :lion
 
-  BUNDLE_ID_PREFIX = "com.oracle.jdk7u25"
+  option 'with-docs', 'Also install SDK documentation'
+  option 'with-unlimited-jce', 'Also install JCE Unlimited Strength Jurisdiction Policy Files'
 
   def options
     [['--target=<target_device>', "Install on a different volume, defaults to '/'"]]
@@ -26,18 +39,26 @@ class JavaSdk < Formula
   def target_device
     # check arguments for a different target device
     ARGV.each do |a|
-      if a.index('--target')
-        return a.sub('--target=', '')
-      end
+      return a.sub('--target=', '') if a.index('--target')
     end
     '/'
   end
 
+  # java is installed under multiple bundle ids for example,
+  # - "/Library/Java/JavaVirtualMachines/jdk1.7.0_25/Contents/Home" go under "com.oracle.jdk7u25"
+  # - "/Library/Internet Plug-Ins/JavaAppletPlugin.plugin" go under "com.oracle.jre"
+  # - "/Library/PreferencePanes/JavaControlPanel.prefPane" go under "com.oracle.jre"
+  def bundle_id_pattern(suffixes = [], prefix = 'com.oracle')
+    # Pattern would be like: "com.oracle.(jdk7u25|jre)"
+    return "#{prefix}.(#{suffixes.join('|')})"
+  end
+
+  # mount dmg, do everything in the block and ensure dmg is unmounted
   def mount_dmg(mountpoint, &block)
     hdiutil_mount mountpoint, Dir['*.dmg'].first
     yield mountpoint if block_given?
   ensure
-    ignore_interrupts{ hdiutil_unmount mountpoint } if mountpoint.exist?
+    ignore_interrupts { hdiutil_unmount mountpoint } if mountpoint.exist?
   end
 
   def hdiutil_mount(mountpoint, dmgfile)
@@ -55,15 +76,36 @@ class JavaSdk < Formula
     safe_system *args
   end
 
-  def installed_files(package)
-    packages = `/usr/sbin/pkgutil --packages --volume #{target_device}`.select { |p| p =~ %r{#{BUNDLE_ID_PREFIX}} }
-    packages.map do |pkg|
-      prefix = `/usr/bin/defaults read /var/db/receipts/#{pkg.chuzzle}.plist InstallPrefixPath`.chuzzle
-      id = `/usr/bin/defaults read /var/db/receipts/#{pkg.chuzzle}.plist PackageIdentifier`.chuzzle
-     `pkgutil --files #{id}`.chuzzle.map do |file|
-        Pathname.new("#{target_device}/#{prefix}/#{file}").cleanpath.to_s.chuzzle
+  def installed_files
+    packages_with_bundle_id(bundle_id_pattern(['jdk7u25', 'jre'])).map do |pkg|
+      prefix = read_receipt(pkg, 'InstallPrefixPath')
+      files_with_bundle_id(read_receipt(pkg, 'PackageIdentifier')).map do |file|
+        Pathname.new("#{target_device}/#{prefix}/#{file}").cleanpath
       end
     end.flatten
+  end
+
+  def jdk_installed_base
+    pkg = packages_with_bundle_id(bundle_id_pattern(['jdk7u25'])).first
+    prefix = read_receipt(pkg, 'InstallPrefixPath') unless pkg.nil? or pkg.empty?
+    Pathname.new("#{target_device}/#{prefix}").cleanpath
+  end
+
+  def packages_with_bundle_id id_prefix
+    (@packages ||= {}).fetch(id_prefix.to_s) do
+      @packages[id_prefix.to_s] =
+        `/usr/sbin/pkgutil --packages --volume #{target_device}`.split("\n").select { |p| p =~ %r{#{id_prefix}} }
+    end
+  end
+
+  def files_with_bundle_id id
+    (@files ||= {}).fetch(id.to_s) do
+      @files[id.to_s] = `/usr/sbin/pkgutil --files #{id}`.split("\n")
+    end
+  end
+
+  def read_receipt(pkg, key)
+    `/usr/bin/defaults read /var/db/receipts/#{pkg}.plist #{key}`.chomp
   end
 
   def install
@@ -72,13 +114,16 @@ class JavaSdk < Formula
       EOS
 
     (prefix+'files.txt').write <<-EOS.undent
-      #{installed_files(BUNDLE_ID_PREFIX).join("\n      ")}
+      #{installed_files.join("\n      ")}
       EOS
 
     mount_dmg(@buildpath/name) do |mount_point|
       pkg_file = Dir["#{mount_point}/*.pkg"].first
-      safe_system "sudo", "installer", "-pkg", pkg_file, "-target", target_device
+      safe_system 'sudo', 'installer', '-pkg', pkg_file, '-target', target_device
     end
+
+    JavaDocs.new.brew { doc.install Dir['*'] } if build.with? 'docs'
+    UnlimitedJcePolicy.new.brew { (prefix + 'jre/lib/security').install Dir['*'] } if build.with? 'unlimited-jce'
   end
 
   def caveats; <<-EOS.undent
@@ -94,12 +139,34 @@ class JavaSdk < Formula
 
     To uninstall, consider manually deleting the files listed in
     #{prefix}/files.txt
+
+    If Java SDK documentation is installed (via '--with-docs'),
+    it can be linked from inside SDK home:
+    sudo ln -s #{doc} \\
+          #{if File.directory?(jdk_installed_base)
+              jdk_installed_base.to_s + '/Contents/Home/docs'
+            else
+              '${JAVA_HOME}/docs'
+            end}
+
+    For more, see: http://www.oracle.com/technetwork/java/javase/javase7-install-docs-439822.html
+
+    If JCE Unlimited Strength Jurisdiction Policy Files is installed (via '--with-unlimited-jce'),
+    the strong policy files present in:
+    #{if File.directory?(jdk_installed_base)
+        jdk_installed_base.to_s + '/Contents/Home/jre/lib/security/{US_export_policy,local_policy}.jar'
+      else
+        '${JAVA_HOME}/jre/lib/security/{US_export_policy,local_policy}.jar'
+      end}
+    can be replaced with the unlimited strength policy files installed in:
+    #{prefix + 'jre/lib/security'}/{US_export_policy,local_policy}.jar
+
+    For more, see: #{prefix + 'jre/lib/security'}/README.txt
     EOS
   end
 
   test do
     java_home = `/usr/libexec/java_home -v '#{version}'`.chuzzle
-    system "#{java_home}/bin/java", "-version"
+    system "#{java_home}/bin/java", '-version'
   end
-
 end
