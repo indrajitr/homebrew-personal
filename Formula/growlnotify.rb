8340dd3274a3851c4898d2b2d345a4f00d4e76e2
Indrajit Raychaudhuri <irc@indrajit.com>
2015-07-09 17:55:56 -0500

    growlnotify: Modernize formula and simplify version detection
    Changes:
    * Move to sha256 based validation
    * Add description property
    * No need for separate `min_version` attribute, reuse `version` instead
    * Rewrite GrowlHelperApp version (`helper_version`) detection to
      attempt 'mdls' (Spotlight) first followed by 'pkgutil'. No need to
      read 'Info.plist' separately, 'mdls' got us covered.

--- Formula/growlnotify.rb
+++ Formula/growlnotify.rb
@@ -3,48 +3,52 @@ require 'formula'
 class GrowlHelperAppRequirement < Requirement
 
   GROWL_HELPER_BUNDLE_ID = "com.Growl.GrowlHelperApp"
-  GROWL_HELPER_BUNDLE_PATH = Pathname.new("/Applications/Growl.app")
-  attr_reader :min_version
 
   fatal true
 
   def initialize(version = "2.1", tags = [])
-    # Extract the min_version if given. Default to GrowlHelperApp 2.x
-    if /(\d+\.)*\d+/ === version.to_s
-      @min_version = Version.new(version)
-    else
-      raise "Invalid version specification for GrowlHelperApp: '#{version}'"
-    end
+    # Extract the version if given. Default to GrowlHelperApp 2.x
+    @version = Version.new(version) if /(\d+\.)*\d+/ === version.to_s
     super tags
   end
 
   satisfy :build_env => false do
-    helper_version ||= begin
-      if File.executable? "#{GROWL_HELPER_BUNDLE_PATH}/Contents/MacOS/Growl"
-        # Try detecting version directly if it is installed in usual place
-        info = "#{GROWL_HELPER_BUNDLE_PATH}/Contents/Info.plist"
-        if File.exist? "#{info}"
-          Version.new(Utils.popen_read(
-            "/usr/bin/defaults", "read", info.to_s, "CFBundleVersion", &:read
-          ).strip)
-        end
-      else
-        # Ask Spotlight where Growl is. If the user didn't installed Growl
-        # in a non-conventional place, this is our only option.
-        # See: http://superuser.com/questions/390757
-        path = MacOS.app_with_bundle_id(GROWL_HELPER_BUNDLE_ID)
-        if not path.nil? and path.exist?
-          Version.new(Utils.popen_read(
-            "/usr/bin/mdls", "-raw", "-nullMarker", "", "-name", "kMDItemVersion", path.to_s, &:read
-          ).strip)
-        end
-      end
+    helper_version ||= detect_version
+    @version <= helper_version unless helper_version.nil?
+  end
+
+  # This technique is mostly taken from 'xcode.rb' in Homebrew library.
+  def detect_version
+    if (path = bundle_path) && path.exist? && (version = version_from_mdls(path))
+      Version.new(version)
+    else
+      Version.new(version_from_pkgutil)
     end
-    @min_version <= helper_version unless helper_version.nil?
+  end
+
+  def bundle_path
+    MacOS.app_with_bundle_id(GROWL_HELPER_BUNDLE_ID)
+  end
+
+  # Ask Spotlight where Growl is. This is our best option even
+  # if the user installed Growl in a non-conventional location.
+  # See: http://superuser.com/questions/390757
+  def version_from_mdls(path)
+    version = Utils.popen_read(
+      "/usr/bin/mdls", "-raw", "-nullMarker", "", "-name", "kMDItemVersion", path.to_s
+    ).strip
+    version unless version.empty?
+  end
+
+  # Growl Helper *does* have a pkg-info entry, so if we can't get it
+  # from mdls, we can try pkgutil. This is very slow.
+  def version_from_pkgutil
+    version = MacOS.pkgutil_info(GROWL_HELPER_BUNDLE_ID)[/version: (.+)$/, 1]
+    version unless version.empty?
   end
 
   def message; <<-EOS.undent
-    Growl Helper App #{@min_version} or newer is required.
+    Growl Helper App #{@version} or newer is required.
     Install it from the App Store at:
       https://itunes.apple.com/app/growl/id467939042?mt=12
     EOS
@@ -52,9 +56,10 @@ class GrowlHelperAppRequirement < Requirement
 end
 
 class Growlnotify < Formula
-  homepage 'http://growl.info/downloads#generaldownloads'
-  url 'http://growl.cachefly.net/GrowlNotify-2.1.zip'
-  sha1 '7880af1e9e6b1fff1174afe39eeeb59e25cf2674'
+  desc "Send Growl Notifications from the command-line"
+  homepage "http://growl.info/downloads#generaldownloads"
+  url "http://growl.cachefly.net/GrowlNotify-2.1.zip"
+  sha256 "eec601488b19c9e9b9cb7f0081638436518bce782d079f6e43ddc195727c04ca"
 
   depends_on :macos => :lion
   depends_on GrowlHelperAppRequirement.new
